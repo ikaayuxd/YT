226dadfff2e1aa216a75d6402e21a368de1385ff
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2023-08-15 10:27:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # set your telegram bot token
-BOT_TOKEN = ""
+BOT_TOKEN = "5643920525:AAEUu2MmpLHEFdVu8iK46yG8p47EZ-IbIjM"
 tutorial = "BAACAgIAAxkBAAIGQmPrxt24nrWaOytCgu-kUDcUHLseAAK7HwACMh1hS9ol-KxNRCjCLgQ"
